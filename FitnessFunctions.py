3a6ecb7622cf947f4fbc10a16186e0de88631fff
Ronnie <ronnieshafran@gmail.com>
2021-03-14 19:14:36 +0200

    Added Bullseye fitness function

--- FitnessFunctions.py
+++ FitnessFunctions.py
@@ -1,4 +1,33 @@
+from string import printable as printable_characters
+
+
 def absolute_distance_fitness(chromosome, target):
     chromosome.fitness = 0
     for i in range(len(target)):
         chromosome.fitness += abs(ord(chromosome.data[i]) - ord(target[i]))
+
+
+def bullseye_fitness(chromosome, target):
+    chromosome.fitness = len(target) * 2
+    BONUS_LEVEL_1 = 1
+    BONUS_LEVEL_2 = 2
+
+    target_hash = create_char_dict_from_collection(target)
+
+    for i in range(len(chromosome.data)):
+        current_char = chromosome.data[i]
+        if current_char in target_hash.keys():
+            if target[i] == current_char:
+                chromosome.fitness -= BONUS_LEVEL_2
+                if target_hash[current_char] is False:
+                    target_hash[current_char] = True
+            elif target_hash[current_char] is False:
+                target_hash[current_char] = True
+                chromosome.fitness -= BONUS_LEVEL_1
+
+
+def create_char_dict_from_collection(collection):
+    result = {}
+    for char in collection:
+        result[char] = False
+    return result
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from SimpleGeneticAlgorithm import SimpleGeneticAlgorithm
-from FitnessFunctions import absolute_distance_fitness as fitness_func
+from FitnessFunctions import bullseye_fitness as fitness_func
 from MatingFunctions import uniform_point_crossover as mating_func
 
 if __name__ == '__main__':
