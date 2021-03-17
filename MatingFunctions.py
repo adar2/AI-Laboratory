b080d45187935fa94d9bbea10eb294383d9f1f82
Ronnie <ronnieshafran@gmail.com>
2021-03-17 19:11:22 +0200

    new crossovers and mutation functions for n queens' solution

--- MatingFunctions.py
+++ MatingFunctions.py
@@ -1,4 +1,5 @@
-from random import randint, choice
+from random import randint, choice, uniform
+from numpy import copy
 from Chromosome import Chromosome
 
 
@@ -26,3 +27,44 @@ def uniform_point_crossover(parent_1: Chromosome, parent_2: Chromosome):
         chosen_parent = choice([parent_1, parent_2])
         child.data = child.data[:j] + chosen_parent.data[j] + child.data[j + 1:]
     return child
+
+
+def ordered_crossover(parent_1: Chromosome, parent_2: Chromosome):
+    child_data = [None] * 10
+    target_size = len(parent_1.data)
+    start_index = randint(0, target_size - 1)
+    end_index = randint(start_index, target_size - 1)
+    for i in range(start_index, end_index + 1):
+        child_data[i] = parent_1.data[i]
+    for item in parent_2.data:
+        if item in child_data:
+            parent_2.data.remove(item)
+    for i in range(len(child_data)):
+        if child_data[i] is None:
+            child_data[i] = parent_2.data[0]
+            del parent_2.data[0]
+    return Chromosome(parent_1.problem, child_data)
+
+
+def partially_matched_crossover(parent_1: Chromosome, parent_2: Chromosome):
+    target_size = len(parent_1.data)
+    index = randint(0, target_size - 1)
+    num_1 = parent_1.data[index]
+    num_2 = parent_2.data[index]
+    offspring_1_data, offspring_2_data = __generate_offsprings(num_1, num_2, parent_1, parent_2)
+    return Chromosome(parent_1.problem, choice([offspring_1_data, offspring_2_data]))
+
+
+def __generate_offsprings(num_1, num_2, parent_1, parent_2):
+    parent_1_num_1_index = parent_1.data.index(num_1)
+    parent_1_num_2_index = parent_1.data.index(num_2)
+    parent_2_num_1_index = parent_1.data.index(num_1)
+    parent_2_num_2_index = parent_1.data.index(num_2)
+    offspring_1_data = copy(parent_1.data)
+    offspring_1_data[parent_1_num_1_index], offspring_1_data[parent_1_num_2_index] = offspring_1_data[parent_1_num_2_index], \
+                                                                                     offspring_1_data[parent_1_num_1_index]
+    offspring_2_data = copy(parent_2.data)
+    offspring_2_data[parent_2_num_1_index], offspring_2_data[parent_2_num_2_index] = offspring_2_data[parent_2_num_2_index], \
+                                                                                     offspring_2_data[
+                                                                                         parent_2_num_1_index]
+    return offspring_1_data, offspring_2_data
--- MutateFunctions.py
+++ MutateFunctions.py
@@ -9,3 +9,38 @@ def string_mutation(chromosome):
     new_char = list(choice(printable))
     chromosome.data = chromosome.data[:index] + new_char + chromosome.data[index + 1:]
 
+
+def exchange_mutation(chromosome):
+    target_size = len(chromosome.data)
+    index1 = uniform(0, target_size - 1)
+    index2 = uniform(0, target_size - 1)
+    chromosome.data[index1], chromosome.data[index2] = chromosome.data[index2], chromosome.data[index1]
+
+
+def displacement_mutation(chromosome):
+    target_size = len(chromosome.data)
+    start_index = uniform(0, target_size - 1)
+    end_index = uniform(start_index, target_size - 1)
+    displaced_items = chromosome.data[start_index:end_index + 1]
+    del chromosome.data[start_index:end_index + 1]
+    insertion_index = uniform(0, len(chromosome.data))
+    chromosome.data = chromosome.data[:insertion_index] + displaced_items + chromosome.data[insertion_index + 1:]
+
+
+def insertion_mutation(chromosome):
+    target_size = len(chromosome.data)
+    index_to_remove = randint(0, target_size - 1)
+    item_to_insert = chromosome.data[index_to_remove]
+    del chromosome.data[index_to_remove]
+    chromosome.data.insert(uniform(0, len(chromosome.data)), item_to_insert)
+
+
+def inversion_mutation(chromosome):
+    target_size = len(chromosome.data)
+    start_index = randint(0, target_size - 1)
+    end_index = randint(start_index, target_size - 1)
+    displaced_items = chromosome.data[start_index:end_index + 1]
+    displaced_items.reverse()
+    del chromosome.data[start_index:end_index + 1]
+    insertion_index = randint(0, len(chromosome.data))
+    chromosome.data = chromosome.data[:insertion_index] + displaced_items + chromosome.data[insertion_index + 1:]
