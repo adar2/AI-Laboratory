6d6adc036c048d5b482d52a842ecc41289c4d38c
Ronnie <ronnieshafran@gmail.com>
2021-03-17 14:07:39 +0200

    Selection implemented

--- SelectionFunctions.py
+++ SelectionFunctions.py
@@ -1,5 +1,7 @@
 from Constants import TRUNCATION_RATE
 
+def get_fitness_proportional_distribution(population: list) -> list:
+
 
 def truncation_selection(population: list):
     truncate_size = int(len(population) * TRUNCATION_RATE)
