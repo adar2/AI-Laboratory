7d564ee87c64ef90089884dc860bdfca7d5731dd
Ronnie <ronnieshafran@gmail.com>
2021-03-17 14:12:44 +0200

    get_fitness_propotional... added

--- SelectionFunctions.py
+++ SelectionFunctions.py
@@ -1,6 +1,15 @@
 from Constants import TRUNCATION_RATE
 
+
 def get_fitness_proportional_distribution(population: list) -> list:
+    result = []
+    fitness_sum = 0
+    for chromosome in population:
+        fitness_sum += chromosome.fitness
+    for chromosome in population:
+        probability = chromosome.fitness / fitness_sum
+        result.append(probability)
+    return result
 
 
 def truncation_selection(population: list):
