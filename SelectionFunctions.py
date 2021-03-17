67238e465762d0e09c19a2fbe24fcabbb730738f
adar2 <warmundadar@gmail.com>
2021-03-17 15:52:11 +0200

    added RWS and SUS parent selection algorithms

--- SelectionFunctions.py
+++ SelectionFunctions.py
@@ -1,5 +1,6 @@
 from Constants import TRUNCATION_RATE
 import numpy as np
+import random
 
 
 def get_fitness_proportional_distribution(population: list) -> list:
@@ -18,3 +19,32 @@ def get_fitness_proportional_distribution(population: list) -> list:
 def truncation_selection(population: list) -> list:
     truncate_size = int(len(population) * TRUNCATION_RATE)
     return population[:truncate_size]
+
+
+def rws(population: list, offspring: int) -> list:
+    selected = []
+    fpd = get_fitness_proportional_distribution(population)
+    for i in range(offspring):
+        r = random.random()
+        i = 0
+        while fpd[i] < r:
+            i += 1
+        selected.append(population[i])
+    return selected
+
+
+def sus(population: list, offspring: int) -> list:
+    selected = []
+    population_size = len(population)
+    fpd = get_fitness_proportional_distribution(population)
+    step = 1 / offspring
+    start = random.uniform(0, step)
+    points = [start + i * step for i in range(offspring)]
+    for point in points:
+        i = 0
+        while fpd[i] < point:
+            i += 1
+            if i >= population_size:
+                i = 0
+        selected.append(population[i])
+    return selected
