f07153a552f3dd695bf9a093bd0327d1b1e70cfd
adar2 <warmundadar@gmail.com>
2021-04-29 17:30:59 +0300

    currently Backjumping.py has been tested with some files and seems to be working , need to add csp heuristics

--- Algorithms/CSPAlgorithms/Backjumping.py
+++ Algorithms/CSPAlgorithms/Backjumping.py
@@ -11,10 +11,13 @@ class BackJumpingAlgorithm(BaseCSPAlgorithm):
         super().__init__(problem)
         self.stack_depth_call = 0
         self.conflict_set_dict = None
+        self.legal_values_dict = None
 
     def reset(self):
         self.conflict_set_dict = {key: set() for key in self.graph.keys()}
         self.vertices_color_dict = {}
+        self.legal_values_dict = {key: set(color for color in self.color_groups_dict.keys()) for key in
+                                  self.graph.keys()}
         for color_group in self.color_groups_dict.values():
             color_group.clear()
 
@@ -63,36 +66,46 @@ class BackJumpingAlgorithm(BaseCSPAlgorithm):
             if self.is_complete():
                 break
             self.generate_another_color()
-        print(f'Number of colors {len(self.color_groups_dict)}')
+        print(f'Found legal graph coloring with {len(self.color_groups_dict)} colors ')
 
     def backjumping_search(self, depth_call: int):
-        # check for stop case
+        # check whether all vertices has been assigned with color
         if self.is_all_assigned():
-            return True, depth_call, set()  # stop the algorithm and do something
+            return True, depth_call, set()  # stop the algorithm and return
+
         vertex = self.select_unassigned_variable()
         print(f'Current vertex : {vertex} ,Depth call : {depth_call} '
               f',Number of assigned vertices : {len(self.vertices_color_dict)} '
               f', Number of colors : {len(self.color_groups_dict)}')
-        for color in self.color_groups_dict.keys():
+
+        color = 1
+        while color <= max(self.color_groups_dict.keys()):
+
             if self.check_consistency(vertex, color):
                 self.update_vertex(vertex, color)
+
                 result, _, conflict_set = self.backjumping_search(depth_call + 1)
                 if result:
                     return result, depth_call, conflict_set
-                # do the backjumping part here
+
+                # backjump until finding vertex contained in the conflict list
                 # assuming deepest vertex in the conflict set
                 if vertex in conflict_set:
                     # update vertex conflict set
                     conflict_set.remove(vertex)
                     self.conflict_set_dict[vertex] = self.conflict_set_dict[vertex].union(conflict_set)
                     self.reset_vertex_assignment(vertex)
+                    # recheck the current color to check if the new conflict set is affecting the assignment
+                    color -= 1
                 else:
                     return result, depth_call, conflict_set
+
+            color += 1
         return False, depth_call, copy(self.conflict_set_dict[vertex])
 
 
 if __name__ == '__main__':
-    graph, vertices, edges = coloring_problem_file_parsing('fpsol2.i.1.col')
+    graph, vertices, edges = coloring_problem_file_parsing('le450_15a.col')
     p = GraphColoringProblem(graph, vertices, edges)
     a = BackJumpingAlgorithm(p)
     a.run()
