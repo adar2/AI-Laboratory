e25ba144ba35fbb172933bc2854af8c890fa0983
Ronnie <ronnieshafran@gmail.com>
2021-05-23 20:21:51 +0300

    added comments for further talking points

--- Problems/MultiKnapsack.py
+++ Problems/MultiKnapsack.py
@@ -21,9 +21,27 @@ class MultiKnapsack(AbstractSearchProblem):
     def get_sorted_configs(self):
         pass
 
+    # this is tricky: we need to see how to differentiate between items that could be put in the knapsack in the future and items
+    # that we already decided not to put. possible solution would be each node holding a dict of who was assigned and who wasn't
+    # or maybe use another thing instead of 0 and 1 (for example 2 for items that we decided not to put in)
     def calc_upper_bound(self, config):
+        raise NotImplementedError
+
+    # return a blank list of items
+    def get_initial_config(self):
+        return [0] * len(self.profits_dict)
+
+    # also need to figure out if capacity is a number or a list (probably a list)
+    def calc_remaining_capacity(self, config):
         pass
 
-
-
-
+    def calc_value(self, config):
+        value = 0
+        for item_index in range(len(config)):
+            if config[item_index] == 1:
+                # profits dict should return a list of the current item's profits in each knapsack
+                profits = self.profits_dict[item_index]
+                for profit in profits:
+                    # add up the profits to account for all knapsacks (initial thought, might not be what we want)
+                    value += profit
+        return value
