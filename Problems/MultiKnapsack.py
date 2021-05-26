5e2a1bdaa315095f2dc7d56df02060cf1939bfac
adar2 <warmundadar@gmail.com>
2021-05-26 12:29:47 +0300

    LeastDiscrepancySearch.py im not sure what the hell is going on

--- Problems/MultiKnapsack.py
+++ Problems/MultiKnapsack.py
@@ -1,3 +1,5 @@
+import copy
+
 from Problems.AbstarctSearchProblem import AbstractSearchProblem
 
 
@@ -7,13 +9,14 @@ class MultiKnapsack(AbstractSearchProblem):
         self.profits_dict = profits_dict
         self.weights_list = weights_list
         self.capacities_list = capacities_list
-        self.number_of_knapsacks = len(self.weights_list)
+        self.number_of_knapsacks = len(self.capacities_list)
 
     # should get list of each knapsack's current weight. Finally, the last item of the list should be a list of all items currently
     # chosen
     def printable_data(self, data: list):
         for knapsack_num in range(self.number_of_knapsacks):
-            print(f'Knapsack number {knapsack_num}:\nCapacity:{data[knapsack_num]} / {self.weights_list[knapsack_num]}\n')
+            print(
+                f'Knapsack number {knapsack_num}:\nCapacity:{data[knapsack_num]} / {self.weights_list[knapsack_num]}\n')
         print('\n')
         print(f'Items in knapsacks:\n {data[-1]}\n')
 
@@ -29,19 +32,39 @@ class MultiKnapsack(AbstractSearchProblem):
 
     # return a blank list of items
     def get_initial_config(self):
-        return [0] * len(self.profits_dict)
+        return [None] * len(self.weights_list)
 
     # also need to figure out if capacity is a number or a list (probably a list)
     def calc_remaining_capacity(self, config):
-        pass
+        capacities_copy = copy.copy(self.capacities_list)
+        for i in range(len(config)):
+            if config[i] is  None:
+                break
+            elif config[i] == 1:
+                for j in range(len(capacities_copy)):
+                    # capacities_copy[j] -= self.weights_list[i]
+                    capacities_copy[j] -= self.profits_dict[j][i]
+        return min(capacities_copy)
 
     def calc_value(self, config):
         value = 0
         for item_index in range(len(config)):
             if config[item_index] == 1:
                 # profits dict should return a list of the current item's profits in each knapsack
-                profits = self.profits_dict[item_index]
-                for profit in profits:
+                for knapsack_profits in self.profits_dict.values():
                     # add up the profits to account for all knapsacks (initial thought, might not be what we want)
-                    value += profit
+                    value += knapsack_profits[item_index]
         return value
+
+    def expand(self, config) -> list:
+        children = []
+        for i in range(len(config)):
+            if config[i] is None:
+                first = copy.copy(config)
+                second = copy.copy(config)
+                first[i] = 0
+                second[i] = 1
+                children.append(second)
+                children.append(first)
+                break
+        return children
